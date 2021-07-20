b2650451f63bf47fc6c19dc2d86021609815ea23
Paul Montecot Grall <paul.montecotgrall@cri-paris.org>
2021-07-20 12:02:33 +0200

    Ana Modif Update

--- main_Ana.py
+++ main_Ana.py
@@ -4,6 +4,7 @@ import plot
 import pandas as pd
 import MathUtilities
 import os
+import numpy as np
 # Put the path of the directory where the data is located (keep the r before the string)
 path = r'C:\Users\CRI User\Documents\GitHub\PythonDFTMotionP3\goodata\Openclose'
 
@@ -14,40 +15,36 @@ def runfeaturesextract():
 
         # DataFrame Creation with the Data
         df = pd.DataFrame()
-        df = df.append(pd.Dataframe(getFeaturesFromFile.datafromfile(path+'\\'+filename)), ignore_index=True)
+        df = df.append(pd.DataFrame(getFeaturesFromFile.datafromfile(path+'\\'+filename)), ignore_index=True)
         df.columns = ['time', 'accX', 'accY', 'accZ', 'gyrX', 'gyrY', 'gyrZ', 'magX', 'magY', 'magZ']
-        # Datatype is the axis of data you want, you just have to replace by the correpsonding column. (exemple 'accX')
-        datatype = ['accX']
 
-        # Operations for Different versions of Data
-        data = df[datatype]
-        df['norme'] = MathUtilities.norme(df)
-        norme = df['norme']
-        df['integral'] = MathUtilities.integral(df, data)
-        integral = df['integral']
-        df['derivate'] = MathUtilities.derivData(df, data)
-        derivate = df['derivate']
-        angle = MathUtilities.angle(df, df.accX, df.accZ)
+    # Datatype is the axis of data you want, you just have to replace by the correpsonding column. (exemple 'accX')
+    datatype = ['accX']
 
-        # Get Features by Window, here you have to specify the type of data you want (data, norme, integral, derivate or angle).
-        # The model is trained for the norme Data so plz extract from norme :)
-        Xfinaldata, Yfinaldata = getFeaturesWindows.getFeaturesWindows(data, 1)
+    # Operations for Different versions of Data
+    Data = df[datatype]
+    df['norme'] = MathUtilities.norme(df)
+    norme = df['norme']
 
-        # Create final DataFrame
-        dffinaldata= pd.DataFrame(Xfinaldata)
-        dffinaldata.columns = ['DC', 'energy', 'entropyDFT', 'Deviation']
+    # Get Features by Window
+    # Choose Data or Norme :)
+    Xfinaldata, Yfinaldata = getFeaturesWindows.getFeaturesWindows(norme, 1)
 
-        # This show the final dataframe in the interpreter
-        print(dffinaldata)
+    # Create final DataFrame
+    dffinaldata = pd.DataFrame(Xfinaldata)
+    dffinaldata.columns = ['DC', 'energy', 'entropyDFT', 'Deviation']
 
-        # This plot the little dashboard with the distributions of values
-        plot.plotfeatures(dffinaldata)
+    # This show the final dataframe in the interpreter
+    print(dffinaldata)
+
+    # This plot the little dashboard with the distributions of values
+    plot.plotfeaturesnotarget(dffinaldata)
 
         # This command create the new file with extracted features.
         # You could replace the path with where you want the file to be stocked in.
         # after the last \ you just have to write the name you want for the file and finish by .csv.
         # Then, you can click on the Play Button :)
-        dffinaldata.to_csv(r'C:\Users\CRI User\Desktop\DataSets\openclosepierro.csv')
+    #dffinaldata.to_csv(r'C:\Users\CRI User\Desktop\DataSets\openclosepierro.csv')
 
 runfeaturesextract()
 
