d73f25df468129bbc637d433495296e25e2e1124
Paul Montecot Grall <paul.montecotgrall@cri-paris.org>
2021-08-24 12:26:18 +0200

    downloading button

--- interface.py
+++ interface.py
@@ -20,20 +20,21 @@ st.markdown("**Fill the differents items then click on generate**")
 st.subheader('User ID')
 user_id = st.text_input("")
 st.sidebar.subheader("Data type")
-datatype = st.sidebar.selectbox("",('accX','accY','accZ'))
+datatype = st.sidebar.selectbox("",('accX','accY','accZ','Norme'))
 st.subheader('Please Upload Data \U00002B07')
 uploaded_file = st.file_uploader("")
 if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     dataframe.columns = ['time', 'accX', 'accY', 'accZ', 'gyrX', 'gyrY', 'gyrZ', 'magX', 'magY', 'magZ']
+    dataframe['Norme'] = MathUtilities.norme(dataframe)
+    Norme = dataframe['Norme']
     st.write(dataframe)
     Data = dataframe[datatype]
-    dataframe['norme'] = MathUtilities.norme(dataframe)
-    norme = dataframe['norme']
     Xfinaldata, Yfinaldata = getFeaturesWindows.getFeaturesWindows(Data, 1)
     dffinaldata = pd.DataFrame(Xfinaldata)
     #dffinaldata.columns = ['DC', 'energy', 'entropyDFT', 'Deviation','contact','pressure']
     dffinaldata.columns = ['DC', 'energy', 'entropyDFT', 'Deviation']
+
 else:
     dffinaldata = pd.DataFrame()
 
@@ -42,7 +43,7 @@ BHK_score = st.text_input(" ")
 st.subheader('Handedness')
 handed = st.selectbox(
 '',
-('Left Handed', 'Right Handed'))
+('Right Handed', 'Left Handed'))
 st.subheader('Level of constraint')
 constraint = st.selectbox("",('1','2','3','4','5','6','7','8','9','10'))
 st.subheader('Constraint type')
@@ -57,10 +58,10 @@ def get_dataBHK():
     return[]
 def get_dataglobal():
     return[]
-mean = dffinaldata.mean(axis=1)
-median = dffinaldata.median(axis=1)
-standard_deviation = dffinaldata.std(axis=1)
-variance = dffinaldata.var(axis=1)
+mean = dffinaldata.mean(axis=0)
+median = dffinaldata.median(axis=0)
+standard_deviation = dffinaldata.std(axis=0)
+variance = dffinaldata.var(axis=0)
 df_BHK = pd.DataFrame()
 df_Global = pd.DataFrame()
 
@@ -72,7 +73,7 @@ elif finger == True:
 if wrist == False:
     wrist = "\U0001F534"
 elif wrist == True:
-    wirst = "\U0001F7E2"
+    wrist = "\U0001F7E2"
 
 if elbow == False:
     elbow = "\U0001F534"
@@ -85,10 +86,12 @@ elif shoulder == True:
     shoulder = "\U0001F7E2"
 
 if st.button('GENERATE'):
-    dataBHK = {"BHK score": BHK_score, "handedness": handed, "Constraint": constraint,"finger": finger, "wrist":wrist,"elbow":elbow,"shoulder":shoulder}
+    dataBHK = {"User ID" : user_id,"BHK score": BHK_score, "handedness": handed, "Constraint": constraint,"finger": finger, "wrist":wrist,"elbow":elbow,"shoulder":shoulder}
     dataglobal = {"Mean": mean, "Median": median, "standard deviation": standard_deviation, "variance": variance}
     df_BHK = pd.DataFrame.from_records([dataBHK])
     df_Global = pd.DataFrame.from_records(dataglobal)
+    dffinaldata["contact"] = True
+    dffinaldata["pressure"] = "NaN"
     df_Frames = dffinaldata
 
     st.dataframe(df_BHK)
@@ -99,9 +102,18 @@ if st.button('GENERATE'):
 
     df_final = pd.concat([df_BHK, df_Global, df_Frames], axis=0)
 
-if st.button('Download'):
-    csv = dffinaldata.to_csv(index=False)
+#if st.button('Download'):
+    csv = df_Frames.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     st.markdown('### **⬇️ Download output CSV File **')
     href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as ".csv")'
     st.markdown(href, unsafe_allow_html=True)
+
+# def get_binary_file_downloader_html(bin_file, file_label='File'):
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     bin_str = base64.b64encode(data).decode()
+#     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+#     return href
+#
+# st.markdown(get_binary_file_downloader_html('data.csv', 'My Data'), unsafe_allow_html=True)
